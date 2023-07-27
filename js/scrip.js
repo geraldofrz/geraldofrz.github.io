db034ec96df28e6f3e07b3550eda72a421c943d9
Geraldo Frazão <geraldoneto39@gmail.com>
2023-07-27 16:29:08 -0300

    plugin de animações e javascript da pagina de bicicletas

--- js/scrip.js
+++ js/scrip.js
@@ -46,3 +46,25 @@ function eventoPerguntas(pergunta) {
 perguntas.forEach(eventoPerguntas);
 
 //Galeria de bicicletas
+const galeria = document.querySelectorAll(".bicicleta-img img");
+const galeriaContainer = document.querySelector(".bicicleta-img");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventoGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventoGaleria);
+
+//Animações
+
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
